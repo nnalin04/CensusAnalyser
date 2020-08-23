fc2e8ec46b80cd42d5ca8604ede9c085859e1e37
nnalin04 <nnalin04@gmail.com>
2020-08-24 01:39:28 +0530

    Refactored th code to take state Census DAO

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
@@ -17,14 +18,21 @@ import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
 
-    List<IndiaCensusCSV> censusCSVList = null;
+    List<IndiaCensusDAO> censusList = null;
     List<IndiaStateCodeCSV> stateCSVList = null;
 
+    public CensusAnalyser() {
+        this.censusList = new ArrayList<IndiaCensusDAO>();
+    }
+
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            censusCSVList = csvBuilder.getCSVFileList(reader, IndiaCensusCSV.class);
-            return censusCSVList.size();
+            Iterator<IndiaCensusCSV> csvFileIterator = csvBuilder.getCSVFileIterator(reader, IndiaCensusCSV.class);
+            while (csvFileIterator.hasNext()) {
+                this.censusList.add(new IndiaCensusDAO(csvFileIterator.next()));
+            }
+            return censusList.size();
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -53,37 +61,37 @@ public class CensusAnalyser {
     }
 
     public String getStateWiseSortedsetsData() {
-        Comparator<IndiaCensusCSV> censusComparator = Comparator.comparing(census -> census.state);
-        censusCSVList = this.sort(censusCSVList, censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(censusCSVList);
+        Comparator<IndiaCensusDAO> censusComparator = Comparator.comparing(census -> census.state);
+        this.sort(censusList, censusComparator);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
         return sortedStateCensusJson;
     }
 
     public String getStateCodeWiseSortedsetsData() {
         Comparator<IndiaStateCodeCSV> censusComparator = Comparator.comparing(census -> census.stateCode);
-        stateCSVList = this.sort(stateCSVList, censusComparator);
+        this.sort(stateCSVList, censusComparator);
         String sortedStateCensusJson = new Gson().toJson(stateCSVList);
         return sortedStateCensusJson;
     }
 
     public String getStatePopulationWiseSortedsetsData() {
-        Comparator<IndiaCensusCSV> censusComparator = Comparator.comparing(census -> census.population);
-        censusCSVList = this.sort(censusCSVList, censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(censusCSVList);
+        Comparator<IndiaCensusDAO> censusComparator = Comparator.comparing(census -> census.population);
+        this.sort(censusList, censusComparator);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
         return sortedStateCensusJson;
     }
 
     public String getStatePopulationDensityWiseSortedsetsData() {
-        Comparator<IndiaCensusCSV> censusComparator = Comparator.comparing(census -> census.densityPerSqKm);
-        censusCSVList = this.sort(censusCSVList, censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(censusCSVList);
+        Comparator<IndiaCensusDAO> censusComparator = Comparator.comparing(census -> census.densityPerSqKm);
+        this.sort(censusList, censusComparator);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
         return sortedStateCensusJson;
     }
 
     public String getStateAreaWiseSortedsetsData() {
-        Comparator<IndiaCensusCSV> censusComparator = Comparator.comparing(census -> census.areaInSqKm);
-        censusCSVList = this.sort(censusCSVList, censusComparator);
-        String sortedStateCensusJson = new Gson().toJson(censusCSVList);
+        Comparator<IndiaCensusDAO> censusComparator = Comparator.comparing(census -> census.areaInSqKm);
+        this.sort(censusList, censusComparator);
+        String sortedStateCensusJson = new Gson().toJson(censusList);
         return sortedStateCensusJson;
     }
 
