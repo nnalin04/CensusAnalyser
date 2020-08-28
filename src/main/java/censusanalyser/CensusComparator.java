ae7b96ea94f362b8fe2f0347aa833e7725c16a97
nnalin04 <nnalin04@gmail.com>
2020-08-28 23:07:28 +0530

    Refactored code to handle the Single Responsiblity Principle by seperating the comparators and enum

--- src/main/java/censusanalyser/CensusComparator.java
+++ src/main/java/censusanalyser/CensusComparator.java
@@ -1,2 +1,16 @@
-package censusanalyser;public class CensusComparator {
+package censusanalyser;
+
+import java.util.Comparator;
+
+public class CensusComparator {
+
+    public enum Country {
+        INDIA, US
+    }
+
+    static Comparator<CensusDAO> getState = Comparator.comparing(census -> census.state);
+    static Comparator<CensusDAO> getStateCode = Comparator.comparing(census -> census.stateCode);
+    static Comparator<CensusDAO> getPopulation = Comparator.comparing(census -> census.population);
+    static Comparator<CensusDAO> getPopulationDensity = Comparator.comparing(census -> census.populationDensity);
+    static Comparator<CensusDAO> getTotalArea = Comparator.comparing(census -> census.totalArea);
 }
